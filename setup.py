d33dcb3324101dfa84774a9b04b76fd82738a9a4
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2019-04-04 16:07:36 +0100

    Setup Nesta Data Science cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='gtr_ai_analysis',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An analysis of the diffusion of AI using research funding data',
+    author='nesta',
+    license='MIT',
+)
